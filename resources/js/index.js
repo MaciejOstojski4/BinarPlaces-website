1c91d42bae168c70afe3cbf009a277ac38b2b035
kapjl-22 <kapjl-22@sr.dmcs.pl>
2017-12-16 14:25:47 +0100

    Dodane pobieranie listy kategorii z mock API

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,12 +1,25 @@
 $(function() {
-    $.ajax({
-        type: "GET",
-        url: "http://taste-api.binarlab.com/api/v1/categories",
-        success: function(res) {
-            console.log("sukces");
-        },
-        error: function(error) {
-            console.log(error);
-        }
-    })
+
+    const apiURL = "http://www.mocky.io/v2/5a35174c2f00002a0ce251d2";
+
+    var fetchCategories = function() {
+        $.ajax({
+            type: "GET",
+            url: apiURL,
+            success: function(res) {
+                const $navbar = $(".navbar-list");
+                res.forEach(function(category) {
+                    const liElem = '<li><a href="">'+ category.name +'</a></li>';
+                    $navbar.append(liElem);
+                });
+                $navbar.children().addClass("navbar-list-element");
+                $navbar.find("a").addClass("navbar-list-element-link");
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        })
+    };
+
+    fetchCategories();
 });
