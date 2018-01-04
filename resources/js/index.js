4feaac9b6dc4debd3df60b51e19872f874267bb4
kapjl-22 <kapjl-22@sr.dmcs.pl>
2018-01-04 10:32:26 +0100

    Refactoring - zastosowanie module pattern

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,60 +1,91 @@
-$(function() {
+var apiClient = (function() {
 
     const API_URL = "http://www.mocky.io/v2/";
+
     const CATEGORIES_PATH = "5a3e7f602f0000b714171398";
     const PLACES_PATH = "5a3786243200008600eb6963";
+    const PLACE_REVIEWS = "5a425ea5300000f21a709ded";
 
-    const MAIN_CONTENT_ANCHOR = "#main-content";
+    var fetchCategories = async function() {
+        const result = await $.ajax({
+            type: "GET",
+            url: API_URL + CATEGORIES_PATH
+        });
+        return result;
+    };
 
-    const CARDS_IN_ROW = 3;
-    const CATEGORIES_IN_NAVBAR = 5;
+    var fetchPlaces = async function(categoryID) {
+        const result = await $.ajax({
+            type: "GET",
+            url: API_URL + PLACES_PATH,
+        });
+        return result
+    };
+
+    var fetchPlaceReviews = async function(placeID) {
+        const result = await $.ajax({
+            type: "GET",
+            url: API_URL + PLACE_REVIEWS,
+        });
+        return result;
+    };
+
+    return {
+        fetchCategories: fetchCategories,
+        fetchPlaces: fetchPlaces,
+        fetchPlaceReviews: fetchPlaceReviews
+    }
+})();
+
+var app = (function() {
+
+    var CARDS_IN_ROW = 3;
+    var CATEGORIES_IN_NAVBAR = 5;
 
     var categories = [];
 
-    var init = function() {
-        setDateInFooter();
-        hideElementOnStart();
-        fetchCategories();
-    };
-
-    var setDateInFooter = function() {
+    var _setDateInFooter = function() {
         var date = new Date();
         $("#copyrightInfo").text("Copyright " + date.getFullYear());
     };
 
-    var hideElementOnStart = function() {
+    var _hideElementOnStart = function() {
         $("#content").hide();
         $("#loader").hide();
     };
 
-    var fetchCategories = function() {
-        $.ajax({
-            type: "GET",
-            url: API_URL + CATEGORIES_PATH,
-            success: function(res) {
-                categories = res.sort(sortCategoriesByCount);
-                const $navbar = $(".navbar-list");
-                addCategoriesToNavbar($navbar);
-                addCssToNavbarElem($navbar);
-                addOnClickToNavbarElem($navbar);
-            },
-            error: function(error) {
-                console.log(error);
-            }
-        })
+    var _addCategoriesToNavbar = function($navbar) {
+        _addDefaultCategory($navbar);
+        _addMostPopularCategory($navbar);
+        _addCategoryInDropdown($navbar);
     };
 
-    var sortCategoriesByCount = function(first, second) {
-        if(first.count > second.count) {
-            return -1;
-        } else if(first.count < second.count) {
-            return 1;
-        } else {
-            return 0;
+    var _addDefaultCategory = function($navbar) {
+        const allPlacesCategory = '<li><a href=MAIN_CONTENT_ANCHOR><span>-1</span>Wszystkie</a></li>';
+        $navbar.append(allPlacesCategory);
+    };
+
+    var _addMostPopularCategory = function($navbar) {
+        for(var i=0; i<CATEGORIES_IN_NAVBAR; i++) {
+            const category = categories[i];
+            const liElem = '<li><a href=MAIN_CONTENT_ANCHOR><span>'+category.id+'</span>'+ category.name +'</a></li>';
+            $navbar.append(liElem);
         }
     };
 
-    var sortPlacesByRate = function(first, second) {
+    var _addCssToNavbarElem = function($navbar) {
+        $navbar.children().addClass("navbar-list-element");
+        $navbar.find("a").addClass("navbar-list-element-link");
+        $navbar.find("span").addClass("hidden");
+    };
+
+    var _removeCardsWithPlaces = function() {
+        var $placeContainer = $("#place-card-container");
+        $placeContainer.children().remove();
+        return $placeContainer;
+    };
+
+    var _sortPlacesByRate = function(first, second) {
         if(first.location.rate > second.location.rate) {
             return -1;
         } else if(first.location.rate < second.location.rate) {
@@ -64,26 +95,69 @@ $(function() {
         }
     };
 
-    var addCategoriesToNavbar = function($navbar) {
-        addDefaultCategory($navbar);
-        addMostPopularCategory($navbar);
-        addCategoryInDropdown($navbar);
+    var _prepareCardsWithPlaces = function($placeContainer, allPlaces, categoryID) {
+        var places = _getPlacesByCategory(allPlaces, categoryID);
+        var $row;
+        console.log(places);
+        places.forEach(function(place, index) {
+            if((index % CARDS_IN_ROW) === 0) {
+                $row = _createRowForCard($placeContainer);
+            }
+            const $column = _createColumnForCard($row);
+            const $placeCard = _createPlaceCard($column);
+            _addPlaceInfoToCard($placeCard, place);
+        });
     };
 
-    var addDefaultCategory = function($navbar) {
-        const allPlacesCategory = '<li><a href=MAIN_CONTENT_ANCHOR><span>-1</span>Wszystkie</a></li>';
-        $navbar.append(allPlacesCategory);
+    var _addPlaceInfoToCard = function($card, place) {
+        const placeImgSrc = _resolveCardImgSrc(place.category_id);
+        const category = _getCategoryById(place.category_id);
+        $("<img>", {src: placeImgSrc, width: "100px", height: "100px"})
+            .addClass("img-circle").appendTo($card);
+        $("<h2>").text(place.name).appendTo($card);
+        $("<small>").text(category.name).appendTo($card);
+        $("<h4>").text(place.location.address).appendTo($card);
+        _addRateHolder($card, place);
     };
 
-    var addMostPopularCategory = function($navbar) {
-        for(var i=0; i<CATEGORIES_IN_NAVBAR; i++) {
-            const category = categories[i];
-            const liElem = '<li><a href=MAIN_CONTENT_ANCHOR><span>'+category.id+'</span>'+ category.name +'</a></li>';
-            $navbar.append(liElem);
+    var _addRateHolder = function($card, place) {
+        const $rateHolder = $('<div data-toggle="modal" data-target="#ratesModal">').appendTo($card);
+        const $rates= $('<select id="rates">').appendTo($rateHolder);
+        for(var i=0; i<5; i++) {
+            $('<option value=' + (i+1) + '>').text(i+1).appendTo($rates);
         }
+        $rates.barrating({
+            theme: 'fontawesome-stars-o',
+            initialRating: place.location.rate,
+            readonly: true
+        });
+        $rateHolder.click(_displayPlaceReviews);
     };
 
-    var addCategoryInDropdown = function($navbar) {
+    var _displayPlaceReviews = function(event) {
+        console.log("tegotego");
+        apiClient.fetchPlaceReviews(1).then(data => {
+            console.log(data);
+        })
+    };
+
+    var _addOnClickToNavbarElem = function($navbar) {
+        $navbar.find("li:not(last)").click(function(e) {
+            e.preventDefault();
+            $("#loader").show();
+            const categoryID = $(this).find("span").text();
+            apiClient.fetchPlaces(categoryID).then(data => {
+                var $placesContainer = _removeCardsWithPlaces();
+                const places = data.sort(_sortPlacesByRate);
+                _prepareCardsWithPlaces($placesContainer, places, categoryID);
+                $("#loader").hide();
+                $("#content").show();
+            });
+            window.location = "#main-content";
+        })
+    };
+
+    var _addCategoryInDropdown = function($navbar) {
         const $selectLi = $('<li class="dropdown show"><a href="#" data-toggle="dropdown">Inne</a></li>');
         $navbar.append($selectLi);
         const $dropDownList = $("<div>").addClass("dropdown-menu navbar-dropdown-category").appendTo($selectLi);
@@ -94,75 +168,17 @@ $(function() {
         }
     };
 
-    var addCssToNavbarElem = function($navbar) {
-        $navbar.children().addClass("navbar-list-element");
-        $navbar.find("a").addClass("navbar-list-element-link");
-        $navbar.find("span").addClass("hidden");
-    };
-
-    var addOnClickToNavbarElem = function($navbar) {
-        $navbar.find("li:not(last)").on("click", "*", function(e) {
-            e.preventDefault();
-            $("#loader").show();
-            fetchPlaces($(this).children().text());
-            window.location = "#main-content";
-        })
-    };
-
-    var fetchPlaces = function(categoryID) {
-        var $placesContainer = removeCardsWithPlaces();
-        $.ajax({
-            type: "GET",
-            url: API_URL + PLACES_PATH,
-            success: function(res) {
-                const places = res.sort(sortPlacesByRate);
-                prepareCardsWithPlaces($placesContainer, res, categoryID);
-                $("#loader").hide();
-                $("#content").show();
-            },
-            error: function(error) {
-              console.log(error);
-            }
-      });
-    };
-
-    var prepareCardsWithPlaces = function($placeContainer, allPlaces, categoryID) {
-        var places = getPlacesByCategory(allPlaces, categoryID);
-        var $row;
-        places.forEach(function(place, index) {
-            if((index % CARDS_IN_ROW) === 0) {
-                $row = createRowForCard($placeContainer);
-            }
-            const $column = createColumnForCard($row);
-            const $placeCard = createPlaceCard($column);
-            addPlaceInfoToCard($placeCard, place);
-        });
-    };
-
-    var addPlaceInfoToCard = function($card, place) {
-        const placeImgSrc = resolveCardImgSrc(place.category_id);
-        const category = getCategoryById(place.category_id);
-        $("<img>", {src: placeImgSrc, width: "100px", height: "100px"})
-            .addClass("img-circle").appendTo($card);
-        $("<h2>").text(place.name).appendTo($card);
-        $("<small>").text(category.name).appendTo($card);
-        $("<h4>").text(place.location.address).appendTo($card);
-        addRateHolder($card, place);
-    };
-
-    var addRateHolder = function($card, place) {
-        const $rateHolder= $('<select id="rates">').appendTo($card);
-        for(var i=0; i<5; i++) {
-            $('<option value=' + (i+1) + '>').text(i+1).appendTo($rateHolder);
+    var _sortCategoriesByCount = function(first, second) {
+        if(first.count > second.count) {
+            return -1;
+        } else if(first.count < second.count) {
+            return 1;
+        } else {
+            return 0;
         }
-        $rateHolder.barrating({
-            theme: 'fontawesome-stars-o',
-            initialRating: place.location.rate,
-            readonly: true
-        });
     };
 
-    var resolveCardImgSrc = function(categoryID) {
+    var _resolveCardImgSrc = function(categoryID) {
         switch(categoryID) {
             case 1:
                 return "images/pizza-food.jpeg";
@@ -177,25 +193,19 @@ $(function() {
         }
     };
 
-    var createRowForCard = function($container) {
+    var _createRowForCard = function($container) {
         return $("<div/>").addClass("row").appendTo($container);
     };
 
-    var createColumnForCard = function($row) {
+    var _createColumnForCard = function($row) {
         return $("<div>").addClass("col-md-4").appendTo($row);
     };
 
-    var createPlaceCard = function($column) {
+    var _createPlaceCard = function($column) {
         return $("<div>").addClass("place-card text-center").appendTo($column);
     };
 
-    var removeCardsWithPlaces = function() {
-        var $placeContainer = $("#place-card-container");
-        $placeContainer.children().remove();
-        return $placeContainer;
-    };
-
-    var getPlacesByCategory = function(places, categoryID) {
+    var _getPlacesByCategory = function(places, categoryID) {
         if(categoryID == -1) {
             return places;
         }
@@ -206,7 +216,7 @@ $(function() {
         });
     };
 
-    var getCategoryById = function(categoryID) {
+    var _getCategoryById = function(categoryID) {
         const category = categories.filter(function(cat) {
             if(cat.id == categoryID){
                 return cat;
@@ -215,5 +225,24 @@ $(function() {
         return category[0];
     };
 
-    init();
+
+    var init = function() {
+        _setDateInFooter();
+        _hideElementOnStart();
+        apiClient.fetchCategories().then(data => {
+            categories = data.sort(_sortCategoriesByCount);
+            const $navbar = $(".navbar-list");
+            _addCategoriesToNavbar($navbar);
+            _addCssToNavbarElem($navbar);
+            _addOnClickToNavbarElem($navbar);
+        })
+    };
+
+    return {
+        init: init,
+    }
+})();
+
+$(function() {
+    app.init();
 });
