74f8cdc455db54c05f5e84243fef5a3f3419e74e
MaciejOstojski <m.ostojski@binarapps.com>
2018-03-30 13:02:39 +0200

    Add place image to infoWindow on map

--- app/resources/js/map.js
+++ app/resources/js/map.js
@@ -1,5 +1,7 @@
 const map = (function() {
 
+  const ROOT_URL = "http://taste-api.binarlab.com";
+
   const init =  function() {
     var lodz = {lat: 51.759249, lng: 19.455983};
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -14,7 +16,7 @@ const map = (function() {
         title: place.name
       });
       const infowindow = new google.maps.InfoWindow({
-        content: prepareInfoWindowContent(place)
+        content: prepareInfoWindowContent(place),
       });
       marker.addListener("click", function() {
         infowindow.open(map, marker);
@@ -27,9 +29,14 @@ const map = (function() {
   };
 
   const prepareInfoWindowContent = function(place) {
-    return "<div><div><h3>" + place.name + "</h3></div>" +
-      "<div><p>" + place.location.address + "</p></div>" +
-      "<div><p>Ocena: " + (place.rate ? place.rate : "brak") + "</p></div></div>";
+    const $infoContainer = $("<div>").addClass("map-marker-container");
+    $("<h3>").text(place.name).appendTo($infoContainer);
+    const $contentContainer = $("<div>").appendTo($infoContainer);
+    $("<p>").text(place.location.address).appendTo($contentContainer);
+    $("<p>").text("Ocena: " + (place.rate ? place.rate : "brak")).appendTo($contentContainer);
+    const $imgContainer = $("<div>").addClass("map-marker-img-container").appendTo($contentContainer);
+    $("<img>").attr("src", ROOT_URL + place.picture_url).addClass("map-marker-img").appendTo($imgContainer);
+    return $infoContainer.get(0);
   };
 
   return {
